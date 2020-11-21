006dbcc2cb978519b7bda50ee0affb56c1b625a0
Claire Vita <clairej.vita@gmail.com>
2020-11-20 23:56:47 -0500

    sql seeding

--- schema.sql
+++ schema.sql
@@ -26,12 +26,12 @@ CREATE TABLE employee (
   PRIMARY KEY (id)
 );
 
-INSERT INTO department (name) VALUES ('Sales');
+INSERT INTO department (name) VALUES ('Dev Ops');
 INSERT INTO department (name) VALUES ('Engineering');
 
 INSERT INTO role (title, salary, department_id) VALUES ('Manager', 100000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Junior Engineer', 60000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Senior Sales Rep', 80000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ('Junior Sales Rep', 50000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ('Engineer', 60000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Lead', 80000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ('Intern', 50000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Claire', 'Vita', 1, 1);
