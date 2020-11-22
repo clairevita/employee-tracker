32d11f2312cc52b7f7b9bbe9cdb5daa60dff3749
Claire Vita <clairej.vita@gmail.com>
2020-11-21 21:58:11 -0500

    Full functionality

--- exports/modifyElement.js
+++ exports/modifyElement.js
@@ -4,81 +4,20 @@ var inquirer = require("inquirer");
 
 const runSearch = require('./../tracker.js');
 
-var modifyConnection = mysql.createConnection({
-  multipleStatements: true,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE
-});
 
-modifyConnection.connect(function (err) {
-  if (err) throw err;
-});
 
-async function modifyElement(what) {
+function modifyElement(what) {
+  
   console.log(what)
   if (what == "Employee Roles") {
-    await selectEmp();
-    runSearch;
+    
+  selectEmp();
+   
   }
 }
 function selectEmp() {
-  employees = [];
-   modifyConnection.query("SELECT * from employee", function (err, response) {
-    if (err) throw err;
-    for (let i = 0; i < response.length; i++) {
-      let empEl = response[i].id + ". " + response[i].first_name + " " + response[i].last_name;
-      employees.push(empEl);
-    }
-    inquirer.prompt([{
-      type: "list",
-      name: "employee",
-      message: "Which employee would you like to change?",
-      choices: employees
-    }]).then(function (answer) {
-      emp_Arr = answer.employee.split(". ");
-      emp_Str = emp_Arr[0];
-      emp_Int = parseInt(emp_Str);
-      selectRole(emp_Int);
-    });
-
-  });
+  
 }
 
-function selectRole(employee) {
-  roles = [];
-  modifyConnection.query("SELECT * from role", function (err, response) {
-    for (let i = 0; i < response.length; i++) {
-      let roleEl = response[i].id + ". " + response[i].title + " |  $" + response[i].salary;
-      roles.push(roleEl);
-    }
-    return inquirer.prompt([{
-      type: "list",
-      name: "roles",
-      message: `Select the role you would like to switch the employee to.`,
-      choices: roles
-    }]).then(function (answer) {
-      role_Arr = answer.roles.split(". ");
-      role_Str = role_Arr[0];
-      role_Int = parseInt(role_Str);
-      modifyConnection.query("UPDATE employee SET ? WHERE ?", [
-        {
-          role_id: role_Int
-        },
-        {
-          id: employee
-        }
-      ], function (err, response) {
-        if (err) throw err;
-        console.log("Role change complete!");
-        
-      });
-
-    });
-
-  });
-}
 
 module.exports = modifyElement;
--- tracker.js
+++ tracker.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var dotenv  = require("dotenv").config();
+var dotenv = require("dotenv").config();
 
 
 const addElement = require('./exports/addElement');
@@ -15,35 +15,35 @@ var addConnection = mysql.createConnection({
   database: process.env.DB_DATABASE
 });
 
-addConnection.connect(function(err) {
+addConnection.connect(function (err) {
   if (err) throw err;
 });
 
-  runSearch();
+runSearch();
 
-  function runSearch() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "Add...",
-          "View...",
-          "Modify...",
-          "Exit"
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.action) {
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add...",
+        "View...",
+        "Modify...",
+        "Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
         case "Add...":
           addNew();
           break;
-  
+
         case "View...":
           viewOld();
           break;
-  
+
         case "Modify...":
           modifyOld();
           break;
@@ -51,15 +51,15 @@ addConnection.connect(function(err) {
         case "Exit":
           quit();
           break;
-        }
-      });
-  }
-  function quit(){
-    console.log("Thanks for using my app! -CV");
-    process.exit();
-  }
-  function addNew(){
-    inquirer
+      }
+    });
+}
+function quit() {
+  console.log("Thanks for using my app! -CV");
+  process.exit();
+}
+function addNew() {
+  inquirer
     .prompt({
       name: "action",
       type: "list",
@@ -71,48 +71,112 @@ addConnection.connect(function(err) {
         "Return to Main Menu"
       ]
     })
-    .then(async function(answer) {
-     await addElement(answer);
+    .then(async function (answer) {
+      await addElement(answer);
       runSearch();
     });
-  }
+}
 
-  function viewOld(){
-    inquirer
-      .prompt({
-        name: "action",
+function viewOld() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to view?",
+      choices: [
+        "Departments",
+        "Roles",
+        "Employees",
+        "Return to Main Menu"
+      ]
+    })
+    .then(async function (answer) {
+
+      await viewElement(answer);
+      runSearch();
+
+    });
+}
+
+function modifyOld() {
+  inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to change?",
+    choices: [
+      "Employee Roles",
+      "Return to Main Menu"
+    ]
+  })
+  .then(async function (answer) {
+    if (answer.action != "Employee Roles"){
+      runSearch();
+    } else {
+    var modifyConnection = mysql.createConnection({
+      multipleStatements: true,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE
+    });
+
+    modifyConnection.connect(function (err) {
+      if (err) throw err;
+    });
+    employees = [];
+    modifyConnection.query("SELECT * from employee", function (err, response) {
+      if (err) throw err;
+      for (let i = 0; i < response.length; i++) {
+        let empEl = response[i].id + ". " + response[i].first_name + " " + response[i].last_name;
+        employees.push(empEl);
+      }
+      inquirer.prompt([{
         type: "list",
-        message: "What would you like to view?",
-        choices: [
-          "Departments",
-          "Roles",
-          "Employees",
-          "Return to Main Menu"
-        ]
-      })
-      .then(async function(answer) {
-        
-          await viewElement(answer);
-          runSearch();
-        
-      });
-  }
+        name: "employee",
+        message: "Which employee would you like to change?",
+        choices: employees
+      }]).then(function (answer) {
+        emp_Arr = answer.employee.split(". ");
+        emp_Str = emp_Arr[0];
+        emp_Int = parseInt(emp_Str);
+        roles = [];
+        modifyConnection.query("SELECT * from role", function (err, response) {
+          for (let i = 0; i < response.length; i++) {
+            let roleEl = response[i].id + ". " + response[i].title + " |  $" + response[i].salary;
+            roles.push(roleEl);
+          }
+          return inquirer.prompt([{
+            type: "list",
+            name: "roles",
+            message: `Select the role you would like to switch the employee to.`,
+            choices: roles
+          }]).then(function (answer) {
+            role_Arr = answer.roles.split(". ");
+            role_Str = role_Arr[0];
+            role_Int = parseInt(role_Str);
+            modifyConnection.query("UPDATE employee SET ? WHERE ?", [
+              {
+                role_id: role_Int
+              },
+              {
+                id: emp_Int
+              }
+            ], function (err, response) {
+              if (err) throw err;
+              console.log("Role change complete!");
+              runSearch();
+            });
 
-  function modifyOld(){
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to change?",
-        choices: [
-          "Employee Roles",
-          "Return to Main Menu"
-        ]
-      })
-      .then(async function(answer) {
-          await modifyElement(answer.action);
-          runSearch();
-      });
-  }
+          });
 
-  module.exports = runSearch;
+        });
+      });
+
+    });
+  }
+});
+}
+
+module.exports = runSearch;
