3381c5759740ea28f6bfa2310c838617f9f138ec
Claire Vita <clairej.vita@gmail.com>
2020-11-22 12:23:50 -0500

    seperated seed

--- schema.sql
+++ schema.sql
@@ -25,12 +25,3 @@ CREATE TABLE employee (
   PRIMARY KEY (id)
 );
 
-INSERT INTO department (name) VALUES ('Dev Ops');
-INSERT INTO department (name) VALUES ('Engineering');
-
-INSERT INTO role (title, salary, department_id) VALUES ('Manager', 100000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Engineer', 60000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Lead', 80000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ('Intern', 50000, 2);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Claire', 'Vita', 1, 1);
